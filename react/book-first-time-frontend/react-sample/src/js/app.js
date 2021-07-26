a163c141046450b4e0ca44208b8e3e64f9f79ea6
w-shindo <mujet88x@gmail.com>
2021-07-27 08:34:51 +0900

    css-loader / style-loaderを利用して、cssもWebPack管理下にする

--- react/book-first-time-frontend/react-sample/src/js/app.js
+++ react/book-first-time-frontend/react-sample/src/js/app.js
@@ -1,3 +1,11 @@
 import hello from './hello';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import style from '../css/style.css'
 
 hello();
+
+ReactDOM.render(
+  <h1>Hello, Frontend Engineer!</h1>,
+  document.getElementById('root')
+)
