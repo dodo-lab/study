2c78a0e178cb6e198d54445218ac93cf17c3b2a3
w-shindo <mujet88x@gmail.com>
2021-08-26 21:52:09 +0900

    nullチェックの共通API追加

--- perfect-java/src/main/java/util/UtilFunctions.java
+++ perfect-java/src/main/java/util/UtilFunctions.java
@@ -1,5 +1,7 @@
 package util;
 
+import java.util.function.Consumer;
+
 public final class UtilFunctions {
     private UtilFunctions() {
     }
@@ -9,6 +11,12 @@ public final class UtilFunctions {
         for (var object : objects) {
             System.out.println(no + " : " + object);
             ++no;
+        }
+    }
+
+    public static <T> void safeApply(T value, Consumer<T> func) {
+        if (value != null) {
+            func.accept(value);
         }
     }
 }
