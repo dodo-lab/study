96eb8d6ddd4598236ea22b2eb24e0f22071c23e9
w-shindo <mujet88x@gmail.com>
2021-09-06 22:35:50 +0900

    【Perfect Java】13章 列挙型と定数と不変型 不定コレクションの見直し

--- perfect-java/src/main/java/chapter_13_enum_constant/ImmutableCollection.java
+++ perfect-java/src/main/java/chapter_13_enum_constant/ImmutableCollection.java
@@ -1,30 +1,58 @@
 package chapter_13_enum_constant;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class ImmutableCollection {
-    private final List<String> list;
+    private final List<String> immutableList;
+    private final List<String> normalList;
 
     public ImmutableCollection() {
-        list = List.of("abc", "def", "ghi");
+        // List.ofで生成したリストは不変リストになる
+        immutableList = List.of("abc", "def", "ghi");
+        // new ArrayListで生成したリストは不変リストではない
+        normalList = new ArrayList<>(immutableList);
     }
 
-    public List<String> getList() {
-        // Collections.unmodifiableListメソッドを利用することで、読み取り専用のリストにすることができる
-        return Collections.unmodifiableList(list);
+    // このメソッドで取得したオブジェクトからは変更可能
+    public List<String> getNormalList() {
+        return normalList;
+    }
+
+    // 同じnormalListでも、Collections.unmodifiableListメソッドを利用することで、読み取り専用のリストにすることが可能
+    public List<String> getReadonlyNormalList() {
+        return Collections.unmodifiableList(normalList);
+    }
+
+    // immutableListはList.ofで生成しているため、最初から変更不可能
+    public List<String> getImmutableList() {
+        return immutableList;
     }
 
     public static void main(String[] args) {
         var collection = new ImmutableCollection();
-        var list = collection.getList();
 
-        // リストに要素を追加しようとすると、UnsupportedOperationException例外が起きる
+        // getNormalListは変更可能
+        var normalList = collection.getNormalList();
+        normalList.add("123");
+
+        // getReadonlyNormalListは変更不可能
+        var readonlyNormalList = collection.getReadonlyNormalList();
         try {
-            list.add("123");
-        } catch (UnsupportedOperationException e) {
-            System.out.println("UnsupportedOperationException 例外検知");
-            e.printStackTrace();
+            readonlyNormalList.add("123");
+        } catch (UnsupportedOperationException exception) {
+            System.out.println("readonlyNormalListの例外検知");
+            exception.printStackTrace();
+        }
+
+        // getImmutableListは変更不可能
+        var immutableList = collection.getImmutableList();
+        try {
+            immutableList.add("123");
+        } catch (UnsupportedOperationException exception) {
+            System.out.println("immutableListの例外検知");
+            exception.printStackTrace();
         }
     }
 }
