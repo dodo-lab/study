33d0ab6f1cd0e6d663148b6b05df27142d60d6d4
w-shindo <mujet88x@gmail.com>
2021-09-16 22:37:09 +0900

    【Perfect Java】17章 スレッド synchronized

---
+++ perfect-java/src/main/java/chapter_17_thread/SynchronizedTest.java
@@ -0,0 +1,71 @@
+package chapter_17_thread;
+
+import util.Exec;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class SynchronizedTest {
+    private static class MyCounter {
+        private int safeCount = 0;
+        private int safeCount2 = 0;
+        private int unsafeCount = 0;
+
+        public void unsafeIncrement() {
+            unsafeCount++;
+        }
+
+        public synchronized void safeIncrement() {
+            safeCount++;
+        }
+
+        public void safeIncrement2() {
+            synchronized (this) {
+                safeCount2++;
+            }
+        }
+
+        public void show() {
+            System.out.println(safeCount);
+            System.out.println(safeCount2);
+            System.out.println(unsafeCount);
+        }
+    }
+
+    private static class Worker implements Runnable {
+        private static final int NUM_LOOP = 10000000;
+        private final Exec exec;
+
+        private Worker(Exec exec) {
+            this.exec = exec;
+        }
+
+        @Override
+        public void run() {
+            for (var i = 0; i < NUM_LOOP; ++i) {
+                exec.exec();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        var executor = Executors.newFixedThreadPool(8);
+        var counter = new MyCounter();
+
+        executor.submit(new Worker(counter::safeIncrement));
+        executor.submit(new Worker(counter::safeIncrement));
+        executor.submit(new Worker(counter::safeIncrement2));
+        executor.submit(new Worker(counter::safeIncrement2));
+        executor.submit(new Worker(counter::unsafeIncrement));
+        executor.submit(new Worker(counter::unsafeIncrement));
+
+        executor.shutdown();
+        try {
+            executor.awaitTermination(10, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        counter.show();
+    }
+}
---
+++ perfect-java/src/main/java/util/Exec.java
@@ -0,0 +1,6 @@
+package util;
+
+@FunctionalInterface
+public interface Exec {
+    void exec();
+}
