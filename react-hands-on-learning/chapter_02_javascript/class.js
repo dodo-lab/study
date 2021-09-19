d9c84c8524b57971a5d5ba3134edae15b4060f85
w-shindo <mujet88x@gmail.com>
2021-09-20 08:32:55 +0900

    【Reactハンズオンラーニング】 2章 クラス

---
+++ react-hands-on-learning/chapter_02_javascript/class.js
@@ -0,0 +1,28 @@
+class Vacation {
+  constructor(destination, length) {
+    this.destination = destination;
+    this.length = length;
+  }
+
+  print() {
+    console.log(`${this.destination} will take ${this.length} days.`);
+  }
+}
+
+const trip = new Vacation('Santiago, Chile', 7);
+trip.print();
+
+class Expedition extends Vacation {
+  constructor(destination, length, gear) {
+    super(destination, length);
+    this.gear = gear;
+  }
+
+  print() {
+    super.print();
+    console.log(`Bring your ${this.gear.join(' and your ')}`);
+  }
+}
+
+const trip2 = new Expedition('Mt. Whitney', 3, ['sunglasses', 'prayer flags', 'camera']);
+trip2.print();
