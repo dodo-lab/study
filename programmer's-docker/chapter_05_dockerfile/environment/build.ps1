f5807f05efcdffebf3f98e51e39f47c5b6f49150
w-shindo <mujet88x@gmail.com>
2021-09-24 08:51:59 +0900

    【プログラマーのためのDocker】5章 Dockerfile 環境変数、Dockerfile内変数、ユーザー等

---
+++ programmer's-docker/chapter_05_dockerfile/environment/build.ps1
@@ -0,0 +1,11 @@
+# ビルド
+docker build -t my-os-image --build-arg YOUR_NAME=oda .
+
+# 実行
+docker run --name=my-os -dit --rm my-os-image
+
+# 環境変数の確認
+docker exec -it my-os /usr/tmp/exec.sh
+
+# 後片付け
+docker stop my-os
