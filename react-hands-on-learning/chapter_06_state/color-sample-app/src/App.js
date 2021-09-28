edc8dbd4aa4366cd520d57bba485703b8c156345
w-shindo <mujet88x@gmail.com>
2021-09-28 10:40:35 +0900

    【Reactハンズオンラーニング】 6章 ステート管理 ステートをコンポーネントツリーの上から下へ伝える

--- react-hands-on-learning/chapter_06_state/color-sample-app/src/App.js
+++ react-hands-on-learning/chapter_06_state/color-sample-app/src/App.js
@@ -1,5 +1,10 @@
+import React, { useState } from 'react';
+import colorData from './color-data.json';
+import ColorList from './components/ColorList';
+
 function App() {
-  return <div className="App"></div>;
+  const [colors, setColors] = useState(colorData);
+  return <ColorList colors={colors} />;
 }
 
 export default App;
---
+++ react-hands-on-learning/chapter_06_state/color-sample-app/src/components/StarRating.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import { FaStar } from 'react-icons/fa';
+
+const Star = ({ selected = false }) => <FaStar color={selected ? 'red' : 'grey'} />;
+
+export default function StarRating({ totalStars = 5, selectedStars = 0 }) {
+  return (
+    <>
+      {/* totalStarsの数分のスターを表示 */}
+      {[...Array(totalStars)].map((_, i) => (
+        <Star key={i} selected={selectedStars > i} />
+      ))}
+      <p>
+        {selectedStars} of {totalStars} stars
+      </p>
+    </>
+  );
+}
