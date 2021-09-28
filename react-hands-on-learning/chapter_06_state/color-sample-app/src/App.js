5a68f2aa3c9f51189a9b855c5e22686cb8f26ea9
w-shindo <mujet88x@gmail.com>
2021-09-28 13:26:21 +0900

    【Reactハンズオンラーニング】 6章 ステート管理 色追加のフォームを実装

--- react-hands-on-learning/chapter_06_state/color-sample-app/src/App.js
+++ react-hands-on-learning/chapter_06_state/color-sample-app/src/App.js
@@ -1,22 +1,39 @@
 import React, { useState } from 'react';
 import colorData from './color-data.json';
+import AddColorFormRef from './components/AddColorFormRef';
 import ColorList from './components/ColorList';
+import { v4 } from 'uuid';
 
 function App() {
   const [colors, setColors] = useState(colorData);
   return (
-    <ColorList
-      colors={colors}
-      onRemoveColor={(id) => {
-        const newColors = colors.filter((color) => color.id !== id);
-        setColors(newColors);
-      }}
-      onRateColor={(id, rating) => {
-        console.log('onRateColor');
-        const newColors = colors.map((color) => (color.id === id ? { ...color, rating } : color));
-        setColors(newColors);
-      }}
-    />
+    <>
+      <AddColorFormRef
+        onNewColor={(title, color) => {
+          const newColors = [
+            ...colors,
+            {
+              id: v4(),
+              rating: 0,
+              title,
+              color,
+            },
+          ];
+          setColors(newColors);
+        }}
+      />
+      <ColorList
+        colors={colors}
+        onRemoveColor={(id) => {
+          const newColors = colors.filter((color) => color.id !== id);
+          setColors(newColors);
+        }}
+        onRateColor={(id, rating) => {
+          const newColors = colors.map((color) => (color.id === id ? { ...color, rating } : color));
+          setColors(newColors);
+        }}
+      />
+    </>
   );
 }
 
