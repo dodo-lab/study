fa683649d78213ccdd446fd7ab225dba301ce4fa
w-shindo <mujet88x@gmail.com>
2021-09-28 10:58:47 +0900

    【Reactハンズオンラーニング】 6章 ステート管理 色の削除機能を追加

--- react-hands-on-learning/chapter_06_state/color-sample-app/src/App.js
+++ react-hands-on-learning/chapter_06_state/color-sample-app/src/App.js
@@ -4,7 +4,15 @@ import ColorList from './components/ColorList';
 
 function App() {
   const [colors, setColors] = useState(colorData);
-  return <ColorList colors={colors} />;
+  return (
+    <ColorList
+      colors={colors}
+      onRemoveColor={(id) => {
+        const newColors = colors.filter((color) => color.id !== id);
+        setColors(newColors);
+      }}
+    />
+  );
 }
 
 export default App;
--- react-hands-on-learning/chapter_06_state/color-sample-app/src/components/ColorList.js
+++ react-hands-on-learning/chapter_06_state/color-sample-app/src/components/ColorList.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import Color from './Color';
 
-export default function ColorList({ colors = {} }) {
+export default function ColorList({ colors = {}, onRemoveColor = (f) => f }) {
   if (!colors.length) {
-    return <div>No Colors Listed.</div>;
+    return <div>No Colors Listed. (Add a Color)</div>;
   }
 
   return (
     <div>
       {colors.map((color, i) => (
-        <Color key={i} {...color} />
+        <Color key={i} {...color} onRemove={onRemoveColor} />
       ))}
     </div>
   );
