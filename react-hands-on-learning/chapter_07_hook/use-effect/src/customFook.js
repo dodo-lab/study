5ce2c5a526209d28b7002f01ada07c9e3a95447e
w-shindo <mujet88x@gmail.com>
2021-09-29 14:16:22 +0900

    【Reactハンズオンラーニング】 7章 フック カスタムフック

---
+++ react-hands-on-learning/chapter_07_hook/use-effect/src/customFook.js
@@ -0,0 +1,18 @@
+import React, { useState, useEffect } from 'react';
+
+export const useJazzyNews = () => {
+  const [posts, setPosts] = useState([]);
+  const addPost = (post) => setPosts((allPosts) => [post, ...allPosts]);
+
+  useEffect(() => {
+    newsFeed.subscribe(addPost);
+    return () => newsFeed.unsubscribe(addPost);
+  }, []);
+
+  useEffect(() => {
+    welcomeChime.play();
+    return () => goodbyeChime.play();
+  });
+
+  return posts;
+};
