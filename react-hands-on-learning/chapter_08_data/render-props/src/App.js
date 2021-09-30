36168a913419068e4a5d09762d46727656528a50
w-shindo <mujet88x@gmail.com>
2021-09-30 22:38:01 +0900

    【Reactハンズオンラーニング】 8章 データ 仮想リスト

--- react-hands-on-learning/chapter_08_data/render-props/src/App.js
+++ react-hands-on-learning/chapter_08_data/render-props/src/App.js
@@ -1,3 +1,5 @@
+import { FixedSizeList } from 'react-window';
+import faker from 'faker';
 import SimpleList from './components/SimpleList';
 
 const tablePeaks = [
@@ -7,9 +9,24 @@ const tablePeaks = [
   { name: 'Mt. Tallac', elevation: 9735 },
 ];
 
+const bigList = [...Array(5000)].map(() => ({
+  name: faker.name.findName(),
+  email: faker.internet.email(),
+  avatar: faker.internet.avatar(),
+}));
+
 function App() {
+  const renderRow = ({ index, style }) => (
+    <div style={{ ...style, ...{ display: 'flex' } }}>
+      <img src={bigList[index].avatar} alt={bigList[index].name} width={50} />
+      <p>
+        {bigList[index].name} - {bigList[index].email}
+      </p>
+    </div>
+  );
+
   return (
-    <div className="App">
+    <div className="App" style={{ display: 'flex' }}>
       <SimpleList
         data={tablePeaks}
         renderEmpty={<p>This list is empty</p>}
@@ -19,6 +36,15 @@ function App() {
           </>
         )}
       />
+
+      <FixedSizeList
+        height={window.innerHeight}
+        width={600}
+        itemCount={bigList.length}
+        itemSize={50}
+      >
+        {renderRow}
+      </FixedSizeList>
     </div>
   );
 }
