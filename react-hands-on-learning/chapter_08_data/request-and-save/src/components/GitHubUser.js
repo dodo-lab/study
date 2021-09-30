ba34ef106b654bf884267b2fe55c5f0ca6ddb582
w-shindo <mujet88x@gmail.com>
2021-09-30 12:05:20 +0900

    【Reactハンズオンラーニング】 8章 データ 非同期リクエストの状態管理

--- react-hands-on-learning/chapter_08_data/request-and-save/src/components/GitHubUser.js
+++ react-hands-on-learning/chapter_08_data/request-and-save/src/components/GitHubUser.js
@@ -5,6 +5,8 @@ const saveJSON = (key, data) => localStorage.setItem(key, JSON.stringify(data));
 
 export default function GitHubUser({ login }) {
   const [data, setData] = useState(loadJSON(`user:${login}`));
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     console.log('useEffect:data', login, data?.login);
@@ -25,23 +27,46 @@ export default function GitHubUser({ login }) {
     if (!login) return;
     if (data && data.login === login) return;
 
+    setLoading(true);
+
     fetch(`https://api.github.com/users/${login}`)
       .then((response) => response.json())
       .then(setData)
-      .catch(console.error);
+      .then(() => new Promise((resolve) => setTimeout(resolve, 1000)))
+      .then(() => setLoading(false))
+      .catch(setError);
   }, [login]);
+
+  if (error) {
+    return <pre>{JSON.stringify(error, null, 2)}</pre>;
+  }
+
+  if (loading) return <h1>loading...</h1>;
 
   if (data) {
     return (
-      <div
-        style={{
-          maxHeight: '200px',
-          overflowY: 'scroll',
-          backgroundColor: 'lightgray',
-        }}
-      >
-        <pre>{JSON.stringify(data, null, 2)}</pre>
-      </div>
+      <>
+        <div className="githubUser" style={{ display: 'flex' }}>
+          <img src={data.avatar_url} alt={data.login} style={{ width: 200 }} />
+          <div>
+            <h1>{data.login}</h1>
+            {data.name && <p>{data.name}</p>}
+            {data.location && <p>{data.location}</p>}
+          </div>
+        </div>
+        <button onClick={() => localStorage.removeItem(`user:${login}`)}>
+          DELETE
+        </button>
+        <div
+          style={{
+            maxHeight: '200px',
+            overflowY: 'scroll',
+            backgroundColor: 'lightgray',
+          }}
+        >
+          <pre>{JSON.stringify(data, null, 2)}</pre>
+        </div>
+      </>
     );
   }
 
