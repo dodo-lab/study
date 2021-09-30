7fe0d810f8d09a9e1346126dba123a4ce37bef93
w-shindo <mujet88x@gmail.com>
2021-09-30 21:50:13 +0900

    【Reactハンズオンラーニング】 8章 データ レンダープロップ

--- react-hands-on-learning/chapter_08_data/render-props/src/App.js
+++ react-hands-on-learning/chapter_08_data/render-props/src/App.js
@@ -1,5 +1,26 @@
+import SimpleList from './components/SimpleList';
+
+const tablePeaks = [
+  { name: 'Freel Peak', elevation: 10891 },
+  { name: 'Monument Peak', elevation: 10067 },
+  { name: 'Pyramid Peak', elevation: 9983 },
+  { name: 'Mt. Tallac', elevation: 9735 },
+];
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <SimpleList
+        data={tablePeaks}
+        renderEmpty={<p>This list is empty</p>}
+        renderItem={(item) => (
+          <>
+            {item.name} - {item.elevation.toLocaleString()}
+          </>
+        )}
+      />
+    </div>
+  );
 }
 
 export default App;
