87a7c28ba7d77bdf013a6b81916700f02f78ffe1
w-shindo <mujet88x@gmail.com>
2021-09-30 10:39:08 +0900

    【Reactハンズオンラーニング】 8章 データ GitHubアカウント情報取得のリクエスト

--- react-hands-on-learning/chapter_08_data/request-and-save/src/App.js
+++ react-hands-on-learning/chapter_08_data/request-and-save/src/App.js
@@ -1,5 +1,11 @@
+import GitHubUser from './components/GitHubUser';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <GitHubUser login="dodo-lab" />
+    </div>
+  );
 }
 
 export default App;
