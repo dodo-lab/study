68a511a91593c435939453527ed4b0758e8a7d62
w-shindo <mujet88x@gmail.com>
2021-10-01 09:45:12 +0900

    【Reactハンズオンラーニング】 8章 データ 描画を抽象化するFetchコンポーネント

--- react-hands-on-learning/chapter_08_data/use-fetch-hook/src/components/GitHubUser.js
+++ react-hands-on-learning/chapter_08_data/use-fetch-hook/src/components/GitHubUser.js
@@ -1,43 +1,36 @@
 import React from 'react';
 import { useFetch } from '../hooks';
+import Fetch from './Fetch';
+
+function UserDetails({ data }) {
+  return (
+    <>
+      <div className="githubUser" style={{ display: 'flex' }}>
+        <img src={data.avatar_url} alt={data.login} style={{ width: 200 }} />
+        <div>
+          <h1>{data.login}</h1>
+          {data.name && <p>{data.name}</p>}
+          {data.location && <p>{data.location}</p>}
+        </div>
+      </div>
+      <div
+        style={{
+          maxHeight: '200px',
+          overflowY: 'scroll',
+          backgroundColor: 'lightgray',
+        }}
+      >
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      </div>
+    </>
+  );
+}
 
 export default function GitHubUser({ login }) {
-  const { loading, data, error } = useFetch(
-    `https://api.github.com/users/${login}`
+  return (
+    <Fetch
+      uri={`https://api.github.com/users/${login}`}
+      renderSuccess={UserDetails}
+    />
   );
-
-  if (error) {
-    return <pre>{JSON.stringify(error, null, 2)}</pre>;
-  }
-
-  if (loading) return <h1>loading...</h1>;
-
-  if (data) {
-    return (
-      <>
-        <div className="githubUser" style={{ display: 'flex' }}>
-          <img src={data.avatar_url} alt={data.login} style={{ width: 200 }} />
-          <div>
-            <h1>{data.login}</h1>
-            {data.name && <p>{data.name}</p>}
-            {data.location && <p>{data.location}</p>}
-          </div>
-        </div>
-        <button onClick={() => localStorage.removeItem(`user:${login}`)}>
-          DELETE
-        </button>
-        <div
-          style={{
-            maxHeight: '200px',
-            overflowY: 'scroll',
-            backgroundColor: 'lightgray',
-          }}
-        >
-          <pre>{JSON.stringify(data, null, 2)}</pre>
-        </div>
-      </>
-    );
-  }
-
-  return null;
 }
