a39ef041bb4bec22000ac53a8c86ee90ef3225f3
w-shindo <mujet88x@gmail.com>
2021-10-02 14:31:47 +0900

    【Reactハンズオンラーニング】 8章 データ リクエスト前の描画制御

--- react-hands-on-learning/chapter_08_data/use-fetch-hook/src/App.js
+++ react-hands-on-learning/chapter_08_data/use-fetch-hook/src/App.js
@@ -6,14 +6,16 @@ import RepositoryReadme from './components/RepositoryReadme';
 
 function App() {
   const [login, setLogin] = useState('dodo-lab');
-  const [repo, setRepo] = useState('sandbox');
+  const [repo, setRepo] = useState();
 
   return (
     <div className="App">
       <SearchForm value={login} onSearch={setLogin} />
-      <GitHubUser login={login} />
-      <UserRepositories login={login} repo={repo} onSelect={setRepo} />
-      <RepositoryReadme login={login} repo={repo} />
+      {login && <GitHubUser login={login} />}
+      {login && (
+        <UserRepositories login={login} repo={repo} onSelect={setRepo} />
+      )}
+      {login && repo && <RepositoryReadme login={login} repo={repo} />}
     </div>
   );
 }
