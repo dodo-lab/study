b5353edef5e69ebc6615e9cc6ae3ec674673f807
w-shindo <mujet88x@gmail.com>
2021-10-06 21:38:14 +0900

    【Reactハンズオンラーニング】 10章 テスト イベントのテスト

---
+++ react-hands-on-learning/chapter_10_test/jest-test/src/components/Checkbox.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useReducer } from 'react';
+
+export default function Checkbox() {
+  const [checked, setChecked] = useReducer((checked) => !checked, false);
+
+  return (
+    <>
+      <label>
+        {checked ? 'checked' : 'not checked'}
+        <input type="checkbox" value={checked} onChange={setChecked} />
+      </label>
+    </>
+  );
+}
---
+++ react-hands-on-learning/chapter_10_test/jest-test/src/components/Checkbox.test.js
@@ -0,0 +1,9 @@
+import { render, fireEvent } from '@testing-library/react';
+import Checkbox from './Checkbox';
+
+test('Selecting the checkbox should change the value of checked to true', () => {
+  const { getByLabelText } = render(<Checkbox />);
+  const checkbox = getByLabelText(/not checked/);
+  fireEvent.click(checkbox);
+  expect(checkbox.checked).toEqual(true);
+});
