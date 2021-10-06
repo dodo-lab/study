4b1233e8bcd1aa8c367f2756f3db423ee8cc87e6
w-shindo <mujet88x@gmail.com>
2021-10-07 07:21:34 +0900

    【Reactハンズオンラーニング】 11章 ルーティング React Router の導入

--- react-hands-on-learning/chapter_11_routing/routing/src/App.js
+++ react-hands-on-learning/chapter_11_routing/routing/src/App.js
@@ -1,5 +1,19 @@
+import React from 'react';
+import { Route, Routes } from 'react-router';
+import { About, Contact, Events, Home, Products } from './pages';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
