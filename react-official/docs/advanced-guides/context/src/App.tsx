8ff7fc8b1117780775484800ec0363d9df429e4c
w-shindo <mujet88x@gmail.com>
2021-10-27 08:52:43 +0900

    ✨: テーマカラーをコンテキスト化して、UserProfileにテーマカラーを適用

--- react-official/docs/advanced-guides/context/src/App.tsx
+++ react-official/docs/advanced-guides/context/src/App.tsx
@@ -2,12 +2,17 @@ import React from 'react';
 import UserProfile from './components/UserProfile';
 import UserSetting from './components/UserSetting';
 import UserProvider from './context/UserProvider';
+import ThemeProvider from './context/ThemeProvider';
+import ThemeSetting from './components/ThemeSetting';
 
 function App() {
   return (
     <UserProvider>
-      <UserProfile />
-      <UserSetting />
+      <ThemeProvider>
+        <UserProfile />
+        <UserSetting />
+        <ThemeSetting />
+      </ThemeProvider>
     </UserProvider>
   );
 }
--- react-official/docs/advanced-guides/context/src/components/UserProfile.tsx
+++ react-official/docs/advanced-guides/context/src/components/UserProfile.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { useTheme } from '../context/ThemeProvider';
 import { useUser } from '../context/UserProvider';
+import './UserProfile.css';
 
 const UserProfile: React.FC = () => {
   const { user } = useUser();
+  const { theme } = useTheme();
 
   return (
-    <div>
+    <div className={theme.color}>
       <h2>User Profile</h2>
       <p>{user.name}</p>
       <p>{user.age}</p>
