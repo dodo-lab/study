25ca8965e5edd47a50eec1ba0a0a2f678bed0a27
w-shindo <mujet88x@gmail.com>
2021-10-29 06:34:39 +0900

    ➕: React Routerを導入

--- react-official/docs/advanced-guides/sandbox/src/App.tsx
+++ react-official/docs/advanced-guides/sandbox/src/App.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <>
+      <BrowserRouter>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
