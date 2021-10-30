178e1e7916b0b07fdb074297910d8b1116603b02
w-shindo <mujet88x@gmail.com>
2021-10-30 21:35:56 +0900

    🩹: フォワーディングコンポーネントの名前がDevToolsで表示されるように

--- react-official/docs/advanced-guides/sandbox/src/pages/forwarding-refs/parts/FancyButton.tsx
+++ react-official/docs/advanced-guides/sandbox/src/pages/forwarding-refs/parts/FancyButton.tsx
@@ -6,8 +6,12 @@ type Props = {
 
 type Ref = HTMLButtonElement;
 
-const FancyButton = React.forwardRef<Ref, Props>(({ children }, ref) => (
-  <button ref={ref}>{children}</button>
-));
+// functionにすると、DevTools上で名前が表示されるようになる
+const FancyButton = React.forwardRef<Ref, Props>(function FancyButton(
+  { children },
+  ref
+) {
+  return <button ref={ref}>{children}</button>;
+});
 
 export default FancyButton;
