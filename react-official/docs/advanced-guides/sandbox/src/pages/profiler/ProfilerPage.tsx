c5aa49601cc480c964f590f7fb41ec69f89353ce
w-shindo <mujet88x@gmail.com>
2021-10-31 19:45:59 +0900

    💡: Profilerコールバック引数にコメントを追加

--- react-official/docs/advanced-guides/sandbox/src/pages/profiler/ProfilerPage.tsx
+++ react-official/docs/advanced-guides/sandbox/src/pages/profiler/ProfilerPage.tsx
@@ -1,6 +1,16 @@
 import { Button } from '@mui/material';
 import React, { Profiler, ProfilerOnRenderCallback } from 'react';
 
+/**
+ * Profilerのコールバック関数
+ * @param id コミットされたProfilerのID
+ * @param phase 'mount'ならツリーの初回マウント、'update'なら再レンダーを意味する
+ * @param actualDuration コミットされた更新のレンダリングに要した時間
+ * @param baseDuration メモ化などの最適化をせずにツリー全体をレンダリングするための推定時間
+ * @param startTime Reactがこの更新のレンダリングを開始した時刻
+ * @param commitTime Reactがこの更新をコミットした時間
+ * @param interactions この更新に属するインタラクションのセット
+ */
 const profilerCallback: ProfilerOnRenderCallback = (
   id,
   phase,
