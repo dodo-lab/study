48d8985f615cf6ffb5135ab119cfe457076468f2
w-shindo <mujet88x@gmail.com>
2021-11-01 09:27:22 +0900

    ✨: DOMノードのinputに対してもref機能を使用

--- react-official/docs/advanced-guides/sandbox/src/pages/ref/RefPage.tsx
+++ react-official/docs/advanced-guides/sandbox/src/pages/ref/RefPage.tsx
@@ -1,8 +1,14 @@
-import { Button, Input } from '@mui/material';
+import { Button, Card, CardContent, CardHeader, Input } from '@mui/material';
 import React, { createRef, useCallback, useEffect } from 'react';
 
 const RefPage: React.FC = () => {
+  const muiInputRef = createRef<HTMLInputElement>();
   const inputRef = createRef<HTMLInputElement>();
+
+  const muiInputFocus = useCallback(() => {
+    console.log('muiInputFocus');
+    muiInputRef.current?.focus();
+  }, [muiInputRef]);
 
   const inputFocus = useCallback(() => {
     console.log('inputFocus');
@@ -10,15 +16,29 @@ const RefPage: React.FC = () => {
   }, [inputRef]);
 
   useEffect(() => {
-    inputFocus();
-  }, [inputFocus]);
+    muiInputFocus();
+  }, [muiInputFocus]);
 
   return (
     <>
-      <Input placeholder="focus" inputRef={inputRef} />
-      <Button variant="contained" onClick={inputFocus}>
-        Focus
-      </Button>
+      <Card>
+        <CardHeader title="Material UI Input × Ref" />
+        <CardContent>
+          <Input placeholder="focus" inputRef={muiInputRef} />
+          <Button variant="contained" onClick={muiInputFocus}>
+            Focus
+          </Button>
+        </CardContent>
+      </Card>
+      <Card sx={{ marginTop: 5 }}>
+        <CardHeader title="input × Ref" />
+        <CardContent>
+          <input placeholder="focus" ref={inputRef} />
+          <Button variant="contained" onClick={inputFocus}>
+            Focus
+          </Button>
+        </CardContent>
+      </Card>
     </>
   );
 };
