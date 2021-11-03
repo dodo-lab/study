816a5e804e205a4bccf7cf7f02a0ef06e438834b
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2021-11-04 08:41:09 +0900

    ✨: usePrevious カスタムフック実装

--- react-official/docs/hooks/sandbox/src/pages/custom-hooks/CustomHooksPage.tsx
+++ react-official/docs/hooks/sandbox/src/pages/custom-hooks/CustomHooksPage.tsx
@@ -1,15 +1,18 @@
 import { Input } from '@mui/material';
 import React from 'react';
 import { useInput } from './useInput';
+import { usePrevious } from './usePrevious';
 
 const CustomHooksPage: React.FC = () => {
   const name = useInput('');
+  const prevName = usePrevious(name.value);
 
   return (
     <>
       <Input {...name} placeholder="name" />
 
       <p>Your name is '{name.value || 'unknown'}'</p>
+      <p>Your previous name is '{prevName || 'unknown'}'</p>
     </>
   );
 };
