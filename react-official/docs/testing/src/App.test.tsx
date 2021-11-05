5250f259213724624f5f3593c5973238fa7d7bc8
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2021-11-05 09:25:35 +0900

    ✅: セットアップと後始末

--- react-official/docs/testing/src/App.test.tsx
+++ react-official/docs/testing/src/App.test.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, RenderResult, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  let app: RenderResult;
+
+  // テストのセットアップ（各種テストAPI実行前に毎回コールされる）
+  beforeEach(() => {
+    app = render(<App />);
+  });
+
+  // テストの後始末（各種テストAPI実行後に毎回コールされる）
+  afterEach(() => {});
+
+  test('renders learn react link', () => {
+    // render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+    expect(app.queryByText('Learn React')).toBeTruthy();
+  });
+
+  test('renders learn react link2', () => {
+    // render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
 });
