ba8594652f4317b2fffbc5593ad47847cfbac763
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2021-11-11 07:51:45 +0900

    ✨: シーケンシャルとパラレルなアニメーションの組み合わせ

--- react-native-official/docs/sandbox/src/screens/various/AnimationScreen.tsx
+++ react-native-official/docs/sandbox/src/screens/various/AnimationScreen.tsx
@@ -2,7 +2,7 @@ import React, {useEffect, useRef} from 'react';
 import {Animated, Easing, StyleSheet, View} from 'react-native';
 import {Button, ButtonGroup, Text} from 'react-native-elements';
 
-const buttons = ['fadeIn', 'fadeOut', 'move'] as const;
+const buttons = ['fadeIn', 'fadeOut', 'move', 'sequence'] as const;
 type ButtonType = typeof buttons[number];
 type ButtonProc = {
   [K in ButtonType]: () => void;
@@ -11,11 +11,21 @@ type ButtonProc = {
 const Screen: React.FC = () => {
   const fadeAnim = useRef(new Animated.Value(0)).current;
   const moveXAnim = useRef(new Animated.Value(0)).current;
+  const sequencePosAnim = useRef(new Animated.ValueXY({x: 0, y: 100})).current;
+  const sequenceRotateAnim = useRef(new Animated.Value(0)).current;
+
+  const sequenceRotateStyle = {
+    rotate: sequenceRotateAnim.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['0deg', '360deg'],
+    }),
+  };
 
   const buttonProc: ButtonProc = {
     fadeIn: () => fade(0, 1),
     fadeOut: () => fade(1, 0),
     move: () => move(150),
+    sequence: () => sequence(),
   };
 
   const fade = (initValue: number, toValue: number) => {
@@ -37,6 +47,29 @@ const Screen: React.FC = () => {
     }).start();
   };
 
+  const sequence = () => {
+    sequencePosAnim.setValue({x: 0, y: 100});
+    sequenceRotateAnim.setValue(0);
+
+    Animated.sequence([
+      Animated.decay(sequencePosAnim, {
+        velocity: {x: 0.5, y: 0},
+        deceleration: 0.997,
+        useNativeDriver: true,
+      }),
+      Animated.parallel([
+        Animated.spring(sequencePosAnim, {
+          toValue: {x: 0, y: 100},
+          useNativeDriver: true,
+        }),
+        Animated.timing(sequenceRotateAnim, {
+          toValue: 1,
+          useNativeDriver: true,
+        }),
+      ]),
+    ]).start();
+  };
+
   const handlePress = (index: number, ...args: any[]) => {
     buttonProc[buttons[index]]();
   };
@@ -49,6 +82,14 @@ const Screen: React.FC = () => {
       </Animated.View>
       <Animated.View style={{...styles.posAnim, transform: [{translateX: moveXAnim}]}}>
         <Text h2>Move</Text>
+      </Animated.View>
+      <Animated.View
+        style={{
+          ...styles.posAnim,
+          // transform: [{translateX: sequencePosAnim.x}, {translateY: sequencePosAnim.y}, sequenceRotateStyle],
+          transform: [...sequencePosAnim.getTranslateTransform(), sequenceRotateStyle],
+        }}>
+        <Text h2>Seq</Text>
       </Animated.View>
     </View>
   );
