1b84cfa4c93752750f0c59d83d95df8e9441c14e
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2021-11-13 11:51:39 +0900

    ✅: Promise

---
+++ jest-official/docs/getting-started/sandbox/src/03-async/promises.test.ts
@@ -0,0 +1,39 @@
+describe('Promises', () => {
+  // テストコードが Promise を返すと、jest はそのpromiseがresolveされるまで待機する
+  // もしくは、promiseがrejectされたらテストを失敗として扱う
+
+  const fetchResolve = () => {
+    return new Promise<string>((resolve) => {
+      resolve('http://abehiroshi.la.coocan.jp/');
+    });
+  };
+
+  const fetchReject = () => {
+    return new Promise<string>((_, reject) => {
+      reject('not found');
+    });
+  };
+
+  test('promise resolve', () => {
+    return fetchResolve().then((data) => {
+      expect(data).toMatch(/abehiroshi/);
+    });
+  });
+
+  test('promise reject', () => {
+    // 想定した数のアサーションが呼ばれることを確認
+    expect.assertions(1);
+    return fetchReject().catch((e) => {
+      expect(e).toMatch('not found');
+    });
+  });
+
+  // .resolves / .rejects のマッチャを使うことで以下のような記述も可能（テスト内容自体は上記2件のテストと同様）
+  test('resolvesマッチャ', () => {
+    return expect(fetchResolve()).resolves.toMatch(/abehiroshi/);
+  });
+
+  test('rejectsマッチャ', () => {
+    return expect(fetchReject()).rejects.toMatch('not found');
+  });
+});
