9dcee845934dc88105a0d138fa22ab24c002e6b4
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2021-11-15 07:47:17 +0900

    ✅: expect.any

---
+++ jest-official/docs/api/sandbox/src/02-expect/expect-any.test.ts
@@ -0,0 +1,15 @@
+/**
+ * expect.any()は、与えられたコンストラクタで生成されたもの全てに一致する。
+ * TypeScriptでは型定義があるため、このパターンは有用とは思えない。
+ */
+
+function randomCall(callback: (number) => void) {
+  return callback(Math.floor(Math.random() * 6 + 1));
+}
+
+test('expect.any', () => {
+  const mock = jest.fn();
+  randomCall(mock);
+  expect(mock).toBeCalledWith(expect.any(Number));
+  expect(1 + 2).toEqual(expect.any(Number));
+});
