c33b8cc50ba4a97580277085e7ae828b7f82d619
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2021-11-14 12:20:28 +0900

    ✅: 待機中のタイマーのみ実行

--- jest-official/docs/guide/sandbox/src/02-timer-mock/timer.test.ts
+++ jest-official/docs/guide/sandbox/src/02-timer-mock/timer.test.ts
@@ -23,4 +23,23 @@ describe('タイマーモック', () => {
 
     expect(callback).toBeCalledTimes(1);
   });
+
+  /**
+   * infinityTimerGameのように再帰的なタイマー処理をしている場合、'jest.runAllTimers()'をコールすると無限ループに陥ってしまう
+   * そのようなケースでは、'jest.runOnlyPendingTimers()'で待機中のタイマーのみを処理するべき
+   */
+  test('待機中のタイマーのみ実行', () => {
+    const callback = jest.fn();
+    infinityTimerGame(callback);
+
+    expect(callback).toBeCalledTimes(0);
+    expect(setTimeout).toHaveBeenCalledTimes(1);
+    expect(setTimeout).toHaveBeenCalledWith(expect.any(Function), 1000);
+
+    jest.runOnlyPendingTimers();
+
+    expect(callback).toBeCalledTimes(1);
+    expect(setTimeout).toHaveBeenCalledTimes(2);
+    expect(setTimeout).toHaveBeenCalledWith(expect.any(Function), 10000);
+  });
 });
