71013a46f5af10ceb6eb79e91517cfca4e5b0b6b
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2021-11-17 06:11:43 +0900

    ✅: mockImplementation

---
+++ jest-official/docs/api/sandbox/src/03-mock/mockFn/mockImplementation.test.ts
@@ -0,0 +1,16 @@
+/**
+ * mockFn.mockImplementation(fn) は、モックの実装として使用する関数を受け取り、
+ * モックがコールされた時に受け取った関数を実行する。
+ *
+ * jest.fn(implementation) は省略形
+ */
+
+describe('mockImplementation', () => {
+  const mockFunc = jest.fn().mockImplementation((scalar) => scalar + 99);
+
+  const a = mockFunc(0);
+  const b = mockFunc(1);
+
+  expect(a).toBe(99);
+  expect(b).toBe(100);
+});
