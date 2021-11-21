322de0b4dfc82c1e897399b3f41f411791346562
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2021-11-21 22:50:39 +0900

    ✨: GestureResponderを使ったタッチ操作

--- react-native-official/docs/sandbox/src/screens/various/GestureResponderScreen.tsx
+++ react-native-official/docs/sandbox/src/screens/various/GestureResponderScreen.tsx
@@ -1,11 +1,56 @@
-import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, {useState} from 'react';
+import {GestureResponderEvent, StyleSheet, View} from 'react-native';
 import {Text} from 'react-native-elements';
 
+class Vec2 {
+  constructor(public x: number, public y: number) {}
+}
+
 const Screen: React.FC = () => {
+  const [currentPos, setCurrentPos] = useState<Vec2>({x: 0, y: -100});
+  const [touchStartPos, setTouchStartPos] = useState<Vec2>({x: 0, y: 0});
+  const [movePos, setMovePos] = useState<Vec2>({x: 0, y: 0});
+
+  const translateX = currentPos.x + (movePos.x - touchStartPos.x);
+  const translateY = currentPos.y + (movePos.y - touchStartPos.y);
+
+  const handleMoveShouldSetResponder = () => {
+    return true;
+  };
+
+  const handleResponderGrant = (event: GestureResponderEvent) => {
+    setTouchStartPos({x: event.nativeEvent.pageX, y: event.nativeEvent.pageY});
+  };
+
+  const handleResponderMove = (event: GestureResponderEvent) => {
+    setMovePos({x: event.nativeEvent.pageX, y: event.nativeEvent.pageY});
+  };
+
+  const handleResponderRelease = () => {
+    setTouchStartPos({x: 0, y: 0});
+    setMovePos({x: 0, y: 0});
+    setCurrentPos({x: translateX, y: translateY});
+  };
+
   return (
     <View style={styles.container}>
-      <Text h1>GestureResponderScreen</Text>
+      <View
+        style={{...styles.box, transform: [{translateX}, {translateY}]}}
+        // style={styles.box}
+        onMoveShouldSetResponder={handleMoveShouldSetResponder}
+        onResponderGrant={handleResponderGrant}
+        onResponderMove={handleResponderMove}
+        onResponderRelease={handleResponderRelease}
+      />
+      <Text>
+        Current: {currentPos.x}, {currentPos.y}
+      </Text>
+      <Text>
+        TouchStart: {touchStartPos.x}, {touchStartPos.y}
+      </Text>
+      <Text>
+        Move: {movePos.x}, {movePos.y}
+      </Text>
     </View>
   );
 };
@@ -15,6 +60,12 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  box: {
+    position: 'absolute',
+    width: 100,
+    height: 100,
+    backgroundColor: 'red',
   },
 });
 
