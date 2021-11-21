c753f1dae8e1abc44369a83f885ead69d1b682ad
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2021-11-21 23:05:41 +0900

    ♻️: リファクタ

--- react-native-official/docs/sandbox/src/screens/various/GestureResponderScreen.tsx
+++ react-native-official/docs/sandbox/src/screens/various/GestureResponderScreen.tsx
@@ -6,13 +6,21 @@ class Vec2 {
   constructor(public x: number, public y: number) {}
 }
 
-const Screen: React.FC = () => {
-  const [currentPos, setCurrentPos] = useState<Vec2>({x: 0, y: -100});
-  const [touchStartPos, setTouchStartPos] = useState<Vec2>({x: 0, y: 0});
-  const [movePos, setMovePos] = useState<Vec2>({x: 0, y: 0});
+const TextVec2: React.FC<{text: string; vec2: Vec2}> = ({text, vec2}) => {
+  return (
+    <Text>
+      {text}：( {Math.round(vec2.x)} , {Math.round(vec2.y)} )
+    </Text>
+  );
+};
 
-  const translateX = currentPos.x + (movePos.x - touchStartPos.x);
-  const translateY = currentPos.y + (movePos.y - touchStartPos.y);
+const Screen: React.FC = () => {
+  const [beforeMovePos, setBeforeMovePos] = useState<Vec2>({x: 0, y: -100});
+  const [touchStartPos, setTouchStartPos] = useState<Vec2>({x: 0, y: 0});
+  const [movingPos, setMovingPos] = useState<Vec2>({x: 0, y: 0});
+
+  const translateX = beforeMovePos.x + (movingPos.x - touchStartPos.x);
+  const translateY = beforeMovePos.y + (movingPos.y - touchStartPos.y);
 
   const handleMoveShouldSetResponder = () => {
     return true;
@@ -23,34 +31,27 @@ const Screen: React.FC = () => {
   };
 
   const handleResponderMove = (event: GestureResponderEvent) => {
-    setMovePos({x: event.nativeEvent.pageX, y: event.nativeEvent.pageY});
+    setMovingPos({x: event.nativeEvent.pageX, y: event.nativeEvent.pageY});
   };
 
   const handleResponderRelease = () => {
     setTouchStartPos({x: 0, y: 0});
-    setMovePos({x: 0, y: 0});
-    setCurrentPos({x: translateX, y: translateY});
+    setMovingPos({x: 0, y: 0});
+    setBeforeMovePos({x: translateX, y: translateY});
   };
 
   return (
     <View style={styles.container}>
       <View
         style={{...styles.box, transform: [{translateX}, {translateY}]}}
-        // style={styles.box}
         onMoveShouldSetResponder={handleMoveShouldSetResponder}
         onResponderGrant={handleResponderGrant}
         onResponderMove={handleResponderMove}
         onResponderRelease={handleResponderRelease}
       />
-      <Text>
-        Current: {currentPos.x}, {currentPos.y}
-      </Text>
-      <Text>
-        TouchStart: {touchStartPos.x}, {touchStartPos.y}
-      </Text>
-      <Text>
-        Move: {movePos.x}, {movePos.y}
-      </Text>
+      <TextVec2 text="Before move position" vec2={beforeMovePos} />
+      <TextVec2 text="Touch start position" vec2={touchStartPos} />
+      <TextVec2 text="Moving position" vec2={movingPos} />
     </View>
   );
 };
