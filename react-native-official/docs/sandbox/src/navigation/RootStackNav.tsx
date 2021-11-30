c5d9075bbf85545b50ee6e1442460d5939ad847b
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2021-12-01 07:42:08 +0900

    ✨: DevSettings実装

--- react-native-official/docs/sandbox/src/navigation/RootStackNav.tsx
+++ react-native-official/docs/sandbox/src/navigation/RootStackNav.tsx
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import * as Screens from 'screens';
+import {DevSettings} from 'react-native';
+import {useNavigation} from '@react-navigation/core';
 
 const Stack = createNativeStackNavigator();
 
 export const RootStackNav: React.FC = () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    DevSettings.addMenuItem('Screen transition to Api', () => {
+      navigation.navigate(Screens.ApiScreen.name);
+    });
+  }, [navigation]);
+
   return (
-    <Stack.Navigator initialRouteName="Top">
+    <Stack.Navigator initialRouteName={Screens.TopScreen.name}>
       <Stack.Screen {...Screens.TopScreen} />
       <Stack.Screen {...Screens.BasicScreen} />
       <Stack.Screen {...Screens.TouchablesScreen} />
