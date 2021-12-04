973a83afccf38b1f3578b011fb32352c09893e6b
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2021-12-04 20:04:44 +0900

    🎨: ナビゲーションに型定義を導入

--- react-navigation-official/sandbox/src/App.tsx
+++ react-navigation-official/sandbox/src/App.tsx
@@ -1,36 +1,50 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import {Button, Text} from 'react-native-elements';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {NavigationContainer, useNavigation} from '@react-navigation/native';
+import {createNativeStackNavigator, NativeStackScreenProps} from '@react-navigation/native-stack';
+import {NavigationContainer} from '@react-navigation/native';
 
-const HomeScreen: React.FC = () => {
-  const navigation = useNavigation();
-
+type HomeProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
+const HomeScreen: React.FC<HomeProps> = ({navigation}) => {
   return (
     <View style={styles.container}>
       <Text h1>Home Screen</Text>
-      <Button title="Go to Details" onPress={() => navigation.navigate('Details')} />
+      <Button title="Go to Details" onPress={() => navigation.navigate('Details', {userId: '1001'})} />
     </View>
   );
 };
 
-const DetailsScreen: React.FC = () => {
+type DetailsProps = NativeStackScreenProps<RootStackParamList, 'Details'>;
+const DetailsScreen: React.FC<DetailsProps> = ({route, navigation}) => {
+  if (!route.params?.userId) throw new Error('userId is undefined');
+  const userId = route.params.userId;
+  const nextUserId = (parseInt(userId) + 1).toString();
+
   return (
     <View style={styles.container}>
       <Text h1>Details Screen</Text>
+      <Text>{userId}</Text>
+      <Button title="Go back" onPress={() => navigation.goBack()} />
+      <Button title="Pop to top" onPress={() => navigation.popToTop()} />
+      <Button title="push Details" onPress={() => navigation.push('Details', {userId: nextUserId})} />
+      <Button title="push Details(none)" onPress={() => navigation.push('Details')} />
     </View>
   );
 };
 
-const Stack = createNativeStackNavigator();
+type RootStackParamList = {
+  Home: undefined;
+  Details: {userId: string} | undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} initialParams={{userId: '1000'}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
@@ -41,6 +55,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
   },
 });
