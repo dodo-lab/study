e47e522c72fdb2cdf7755f92508fea96d5ccc64c
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2021-12-09 19:56:06 +0900

    ✨: Drawerの状態を検知

--- react-navigation-official/sandbox/src/screens/drawer/DrawerFirstScreen.tsx
+++ react-navigation-official/sandbox/src/screens/drawer/DrawerFirstScreen.tsx
@@ -1,15 +1,20 @@
-import {DrawerScreenProps} from '@react-navigation/drawer';
+import {DrawerScreenProps, useDrawerStatus} from '@react-navigation/drawer';
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import {Button, Text} from 'react-native-elements';
 
 type Props = DrawerScreenProps<DrawerParamList, 'DrawerFirst'>;
 const Screen: React.FC<Props> = ({navigation}) => {
+  const isDrawerOpen = useDrawerStatus() === 'open';
+
   return (
     <View style={styles.container}>
       <Text h1>DrawerFirstScreen</Text>
       <Button title="Go to Second" onPress={() => navigation.navigate('DrawerSecond')} />
       <Button title="Open drawer" onPress={() => navigation.openDrawer()} />
+      <Text style={styles.drawerStatus} h4>
+        drawer status : {isDrawerOpen ? 'open' : 'close'}
+      </Text>
     </View>
   );
 };
@@ -19,6 +24,10 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'space-around',
+  },
+  drawerStatus: {
+    alignSelf: 'flex-end',
+    marginRight: 20,
   },
 });
 
