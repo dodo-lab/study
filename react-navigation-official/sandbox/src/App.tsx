f1a8318c621be1cb63b34372ea2514931223a276
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2021-12-14 08:13:07 +0900

    🐛: リリース版でのみ発生する問題の対応

--- react-navigation-official/sandbox/src/App.tsx
+++ react-navigation-official/sandbox/src/App.tsx
@@ -1,3 +1,6 @@
+// Without 'react-native-gesture-handler' import, the app may crash in production even if it works fine in development.
+// https://reactnavigation.org/docs/drawer-navigator#:~:text=Copy-,Note,-%3A%20If%20you%20are
+import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import {WithUser} from 'contexts';
 import {WithInitialize} from 'contexts/InitializeContext';
