6527a0da9bf11d5d5bfbba5b128c196b951c9122
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2021-12-24 11:47:10 +0900

    ✨: Doneボタン押下時に、TextViewに入力したニックネームを設定し、DoneボタンとEditTextを非表示にする

--- kotlin-android-fundamentals/lesson-02/LinearLayout_AboutMe/app/src/main/java/com/example/linearlayout_aboutme/MainActivity.kt
+++ kotlin-android-fundamentals/lesson-02/LinearLayout_AboutMe/app/src/main/java/com/example/linearlayout_aboutme/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.linearlayout_aboutme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.done_button).setOnClickListener { addNickname(it) }
+    }
+
+    private fun addNickname(view: View) {
+        val nicknameEdit = findViewById<EditText>(R.id.nickname_edit)
+        val nicknameText = findViewById<TextView>(R.id.nickname_text)
+
+        nicknameText.text = nicknameEdit.text
+
+        view.visibility = View.GONE
+        nicknameEdit.visibility = View.GONE
+        nicknameText.visibility = View.VISIBLE
     }
 }
