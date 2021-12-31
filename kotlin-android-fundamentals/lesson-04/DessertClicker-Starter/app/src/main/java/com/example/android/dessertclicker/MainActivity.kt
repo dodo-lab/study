e94d9560e36679c515fca3af7bf7022a8e29cc0e
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2021-12-31 18:19:15 +0900

    🔊: 他のライフサイクルにもログを埋め込み

--- kotlin-android-fundamentals/lesson-04/DessertClicker-Starter/app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
+++ kotlin-android-fundamentals/lesson-04/DessertClicker-Starter/app/src/main/java/com/example/android/dessertclicker/MainActivity.kt
@@ -145,6 +145,31 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onResume() {
+        super.onResume()
+        Timber.i("onResume Called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Timber.i("onPause Called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Timber.i("onStop Called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Timber.i("onDestroy Called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Timber.i("onRestart Called")
+    }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.main_menu, menu)
         return super.onCreateOptionsMenu(menu)
