8287262180d325ec02ee07656873959f4da7dd0d
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-01-02 07:24:44 +0900

    ✨: LiveDataの宣言と初期化

--- kotlin-android-fundamentals/lesson-05/GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ kotlin-android-fundamentals/lesson-05/GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -1,20 +1,24 @@
 package com.example.android.guesstheword.screens.game
 
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class GameViewModel : ViewModel() {
     // The current word
-    var word = ""
+    var word = MutableLiveData<String>()
 
     // The current score
-    var score = 0
+    var score = MutableLiveData<Int>()
 
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
 
     init {
         Log.i("GameViewModel", "GameViewModel created!")
+
+        word.value = ""
+        score.value = 0
 
         resetList()
         nextWord()
