60a5d37ceaee1e3d532d21bd645c794841c345c0
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-01-08 15:30:34 +0900

    ✨: Imageタグで画像表示

--- nextjs-official/learn/nextjs-blog/pages/posts/first-post.js
+++ nextjs-official/learn/nextjs-blog/pages/posts/first-post.js
@@ -1,14 +1,21 @@
-import Link from "next/link";
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function FirstPost() {
   return (
     <>
       <h1>First Post</h1>
+      <Image
+        src="/images/profile.jpg"
+        width={144}
+        height={144}
+        alt="Your Name"
+      />
       <h2>
         <Link href="/">
           <a>Back to home</a>
         </Link>
       </h2>
     </>
-  )
+  );
 }
