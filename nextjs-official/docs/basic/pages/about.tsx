4120cd758976474d67f0a18ca7840b0ebe7068e1
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-01-12 06:48:55 +0900

    ✨: ページの静的生成

--- nextjs-official/docs/basic/pages/about.tsx
+++ nextjs-official/docs/basic/pages/about.tsx
@@ -1,7 +1,29 @@
-import React from 'react';
+import {GetStaticProps} from 'next';
+import React, {useEffect} from 'react';
 
-const About: React.FC = () => {
-  return <div>About</div>;
+type Props = {
+  message: string;
+};
+
+const About: React.FC<Props> = ({message}) => {
+  useEffect(() => {
+    console.log('useEffect');
+  }, []);
+  return (
+    <>
+      <h1>About</h1>
+      <p>{message}</p>
+    </>
+  );
+};
+
+export const getStaticProps: GetStaticProps = async context => {
+  console.log('getStaticProps');
+  return {
+    props: {
+      message: 'hello',
+    },
+  };
 };
 
 export default About;
