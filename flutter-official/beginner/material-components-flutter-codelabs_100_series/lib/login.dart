6cfe4c61992caef8594cbdb86773df5990851328
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-02-07 14:33:57 +0900

    ✨: ボタンの設置とキャンセルボタンで入力をクリア

--- flutter-official/beginner/material-components-flutter-codelabs_100_series/lib/login.dart
+++ flutter-official/beginner/material-components-flutter-codelabs_100_series/lib/login.dart
@@ -22,7 +22,10 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
-  // TODO: Add text editing controllers (101)
+  // FIXED: Add text editing controllers (101)
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,6 +48,7 @@ class _LoginPageState extends State<LoginPage> {
             // FIXED: Add TextField widgets (101)
             // [Name]
             TextField(
+              controller: _usernameController,
               decoration: const InputDecoration(
                 filled: true,
                 labelText: 'Username',
@@ -54,13 +58,36 @@ class _LoginPageState extends State<LoginPage> {
             const SizedBox(height: 12.0),
             // [Password]
             TextField(
+              controller: _passwordController,
               decoration: const InputDecoration(
                 filled: true,
                 labelText: 'Password',
               ),
               obscureText: true,
             ),
-            // TODO: Add button bar (101)
+            // FIXED: Add button bar (101)
+            ButtonBar(
+              // TODO: Add a beveled rectangular border to CANCEL (103)
+              children: <Widget>[
+                // FIXED: Add buttons (101)
+                TextButton(
+                  child: const Text('CANCEL'),
+                  onPressed: () {
+                    // FIXED: Clear the text fields (101)
+                    _usernameController.clear();
+                    _passwordController.clear();
+                  },
+                ),
+                // TODO: Add an elevation to NEXT (103)
+                // TODO: Add a beveled rectangular border to NEXT (103)
+                ElevatedButton(
+                  child: const Text('OK'),
+                  onPressed: () {
+                    // TODO: Show the next page (101)
+                  },
+                )
+              ],
+            ),
           ],
         ),
       ),
