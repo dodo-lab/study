99142326920ffd735fc9624ddeb27b2c5769432a
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-02-07 17:35:57 +0900

    ✨: 基本的なナビゲーション

--- flutter-official/beginner/sandbox/lib/cookbook/navigation/navigationBasics.dart
+++ flutter-official/beginner/sandbox/lib/cookbook/navigation/navigationBasics.dart
@@ -22,8 +22,30 @@ class FirstRoute extends StatelessWidget {
         child: ElevatedButton(
           child: const Text('Open route'),
           onPressed: () {
-            // Navigate to second route when tapped.
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const SecondRoute()));
           },
+        ),
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Go back!'),
         ),
       ),
     );
