e90380ad5a0c96074c8470c1421b6bde7e478f51
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-02-19 16:58:33 +0900

    🎉: React Queryプロジェクト

---
+++ react-query-official/src/pages/_app.tsx
@@ -0,0 +1,29 @@
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import {createTheme, CssBaseline, ThemeProvider} from '@mui/material';
+import type {AppProps} from 'next/app';
+
+const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 600,
+      md: 900,
+      lg: 1200,
+      xl: 1800,
+    },
+  },
+});
+
+function MyApp({Component, pageProps}: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
+
+export default MyApp;
---
+++ react-query-official/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import {Container, Typography} from '@mui/material';
+import type {NextPage} from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <Container maxWidth="xl">
+      <Typography variant="h1">Next.js template for mui</Typography>
+    </Container>
+  );
+};
+
+export default Home;
