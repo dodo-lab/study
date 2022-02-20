601e72ed2e801eca0c8e41c43f09d809a4a7dd75
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-02-20 13:25:46 +0900

    ✨: リクエストパラメータとして'delay'を受付る

--- react-query-official/src/pages/api/faker.ts
+++ react-query-official/src/pages/api/faker.ts
@@ -1,11 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import faker from 'faker';
 import type {NextApiRequest, NextApiResponse} from 'next';
+import {assertString} from 'utils/typeGuard';
 
 const name = faker;
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({
-    name: `${faker.name.firstName()} ${faker.name.lastName()}`,
-  });
+  const delayStr = req.query.delay ?? '0';
+  assertString(delayStr);
+
+  const delay = Number.parseInt(delayStr);
+  setTimeout(
+    () =>
+      res.status(200).json({
+        name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+      }),
+    delay,
+  );
 }
