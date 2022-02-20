44f43e0bb89904f10b9be461eebd9ce60b81e3df
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-02-21 06:38:53 +0900

    🐛: フィルター指定時の表示に項目名が含まれてなかったので修正

--- react-query-official/src/components/QueryResult.tsx
+++ react-query-official/src/components/QueryResult.tsx
@@ -2,13 +2,22 @@ import {CircularProgress, Divider, Typography} from '@mui/material';
 import React from 'react';
 import {UseQueryResult} from 'react-query';
 
+type UseQueryResultKey = keyof UseQueryResult<any, unknown>;
+
 type Props = {
   title: string;
   result: UseQueryResult<any, unknown>;
-  filters?: (keyof UseQueryResult<any, unknown>)[];
+  filters?: UseQueryResultKey[];
 };
 
 const QueryResult: React.FC<Props> = ({title, result, filters}) => {
+  const showResult = filters
+    ? filters.reduce<Record<string, any>>((prev, current) => {
+        prev[current] = result[current];
+        return prev;
+      }, {})
+    : result;
+
   return (
     <>
       <Divider sx={{mb: 2}} />
@@ -16,15 +25,7 @@ const QueryResult: React.FC<Props> = ({title, result, filters}) => {
         {title}
         {result.isLoading && <CircularProgress size={22} sx={{ml: 2}} />}
       </Typography>
-      {filters ? (
-        filters.map(filter => (
-          <Typography key={filter} component="pre">
-            {JSON.stringify(result[filter], null, 2)}
-          </Typography>
-        ))
-      ) : (
-        <Typography component="pre">{JSON.stringify(result, null, 2)}</Typography>
-      )}
+      <Typography component="pre">{JSON.stringify(showResult, null, 2)}</Typography>
     </>
   );
 };
