40a73203213dd697d55b103c52f5de4f92d0acd3
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-02-20 11:28:59 +0900

    ✨: useQueryでタイムアウトした場合

--- react-query-official/src/pages/queries.tsx
+++ react-query-official/src/pages/queries.tsx
@@ -11,12 +11,16 @@ const Queries: React.FC = () => {
   const error400 = useQuery('400', async () => {
     return await axios.get('/api/400');
   });
+  const timeout = useQuery('timeout', async () => {
+    return await axios.get('/api/timeout', {timeout: 5000});
+  });
 
   return (
     <Container maxWidth="xl">
       <Typography variant="h2">Queries</Typography>
       <QueryResult title="hello" result={hello} />
       <QueryResult title="error400" result={error400} />
+      <QueryResult title="timeout" result={timeout} />
     </Container>
   );
 };
