fdce6957f3e93c84c2201948682d8c13b7e713e1
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-02-22 06:50:39 +0900

    ✨: faker APIの複数版

---
+++ react-query-official/src/pages/api/fakers.ts
@@ -0,0 +1,29 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import faker from 'faker';
+import type {NextApiRequest, NextApiResponse} from 'next';
+import {assertString} from 'utils/typeGuard';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const delayStr = req.query.delay ?? '0';
+  const cursorStr = req.query.cursor ?? '0';
+  const countStr = req.query.count ?? '0';
+  assertString(delayStr);
+  assertString(cursorStr);
+  assertString(countStr);
+
+  const delay = Number.parseInt(delayStr);
+  const cursor = Number.parseInt(cursorStr);
+  const count = Number.parseInt(countStr);
+
+  return new Promise<void>(resolve => {
+    setTimeout(() => {
+      res.status(200).json(
+        [...Array(count)].map((_, index) => ({
+          index: cursor + index,
+          name: `${faker.name.firstName()} ${faker.name.lastName()}`,
+        })),
+      );
+      resolve();
+    }, delay);
+  });
+}
