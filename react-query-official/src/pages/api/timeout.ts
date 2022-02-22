9fc995dc15f48e2d9904e248e128c1ccc4b83ae7
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-02-23 08:38:55 +0900

    ♻️: Promiseの型付け位置を変更

--- react-query-official/src/pages/api/timeout.ts
+++ react-query-official/src/pages/api/timeout.ts
@@ -1,8 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type {NextApiRequest, NextApiResponse} from 'next';
 
-export default function handler(req: NextApiRequest, res: NextApiResponse): Promise<void> {
-  return new Promise(resolve => {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  return new Promise<void>(resolve => {
     setTimeout(() => {
       res.status(200).json({});
       resolve();
