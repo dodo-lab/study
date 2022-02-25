800a38e2a1fa1b7256027e4c8bcc32706b8aed41
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-02-25 19:28:14 +0900

    ✨: シンプルなクラスを作成

--- dart-official/code-labs/from_java_to_dart/bin/main.dart
+++ dart-official/code-labs/from_java_to_dart/bin/main.dart
@@ -1,3 +1,30 @@
+class Bicycle {
+  int cadence;
+  int gear;
+
+  // 先頭に'_'を付与することで、privateな扱いになる
+  int _speed = 0;
+  int get speed => _speed;
+
+  Bicycle(this.cadence, this.gear);
+
+  void applyBrake(int decrement) {
+    _speed -= decrement;
+  }
+
+  void speedUp(int increment) {
+    _speed += increment;
+  }
+
+  @override
+  String toString() => 'Bicycle: $_speed mph';
+}
+
 void main(List<String> arguments) {
-  print('Hello world!');
+  var bike = Bicycle(2, 1);
+  print(bike);
+  bike.speedUp(60);
+  print(bike);
+  bike.applyBrake(22);
+  print(bike);
 }
