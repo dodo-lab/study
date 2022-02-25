4733ea13c9f2bb87cb868ee9c8286f13ca09186b
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-02-26 07:39:26 +0900

    ✨: forInループ

---
+++ dart-official/code-labs/iterable_collections/bin/reading-elements.dart
@@ -0,0 +1,10 @@
+void forInLoop() {
+  const iterable = ['Salad', 'Popcorn', 'Toast'];
+  for (final element in iterable) {
+    print(element);
+  }
+}
+
+void main(List<String> arguments) {
+  forInLoop();
+}
