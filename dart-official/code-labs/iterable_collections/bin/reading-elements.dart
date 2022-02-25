4e411321299980b80f7b99a60903ec31c0fdba35
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-02-26 07:40:09 +0900

    ✨: firstとlast

--- dart-official/code-labs/iterable_collections/bin/reading-elements.dart
+++ dart-official/code-labs/iterable_collections/bin/reading-elements.dart
@@ -5,6 +5,17 @@ void forInLoop() {
   }
 }
 
+void firstAndLast() {
+  const Iterable<String> iterable = ['Salad', 'Popcorn', 'Toast'];
+
+  // 空のIterableに'first'や'last'でアクセスすると、StateErrorが発生するため、'isNotEmpty'でチェックしておく
+  if (iterable.isNotEmpty) {
+    print('The first element is ${iterable.first}');
+    print('The last element is ${iterable.last}');
+  }
+}
+
 void main(List<String> arguments) {
   forInLoop();
+  firstAndLast();
 }
