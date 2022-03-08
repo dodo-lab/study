10e2719ad4e39d5a838d529fa9acbb04ca484c27
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-03-09 07:46:23 +0900

    ✨: Getting an object’s type

--- dart-official/language/bin/tour/classes/using_constructors.dart
+++ dart-official/language/bin/tour/classes/using_constructors.dart
@@ -7,7 +7,7 @@ void constantConstructors() {
   final c2 = const Point(1, 1);
 
   assert(!identical(p1, p2));
-  assert(!identical(c1, c1));
+  assert(!identical(p1, c1));
 
   // 同じパラメータを持つコンパイル時定数は同一オブジェクトとなる
   assert(identical(c1, c2));
@@ -19,6 +19,7 @@ void constantMap() {
     'line': [Point(1, 10), Point(-2, 11)],
   };
   print(pointAndLine);
+  print('The type of pointAndLine is ${pointAndLine.runtimeType}');
 
   // 定数のため変更不可
   // pointAndLine['line']?[0].x = 100;
