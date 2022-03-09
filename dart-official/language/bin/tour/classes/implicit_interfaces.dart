25efcba2d99dfcd4d5e444aa59541cc06e16238c
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-03-10 07:54:54 +0900

    ✨: Implicit interfaces

---
+++ dart-official/language/bin/tour/classes/implicit_interfaces.dart
@@ -0,0 +1,22 @@
+class Person {
+  final String _name;
+
+  Person(this._name);
+
+  String greet(String who) => 'Hello, $who. I am $_name';
+}
+
+class Imposter implements Person {
+  @override
+  String get _name => '';
+
+  @override
+  String greet(String who) => 'Hi $who. Do you know who I am?';
+}
+
+String greetBob(Person person) => person.greet('Bob');
+
+void main() {
+  print(greetBob(Person('Kathy')));
+  print(greetBob(Imposter()));
+}
