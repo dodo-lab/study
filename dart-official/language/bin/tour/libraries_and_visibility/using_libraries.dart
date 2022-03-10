17a2cf9ec7c24df5e71ed06d3b1276076c505d7b
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-03-11 08:37:45 +0900

    ✨: Using libraries

---
+++ dart-official/language/bin/tour/libraries_and_visibility/using_libraries.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: unused_import
+
+// 'as'で別名を付与することが可能（APIの競合回避）
+import 'dart:html' as html;
+
+// 'show'でライブラリの一部だけを利用
+import 'dart:math' show pi, e;
+
+// 'hide'でライブラリの一部を利用しない
+import 'dart:collection' hide HashMap, LinkedList;
+
+void main() {}
