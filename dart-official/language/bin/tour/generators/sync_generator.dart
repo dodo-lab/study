fb0ff05a9baa18cf316d63e355e65b4623ff8092
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-03-12 09:25:33 +0900

    ✨: Generator using recursive calls

--- dart-official/language/bin/tour/generators/sync_generator.dart
+++ dart-official/language/bin/tour/generators/sync_generator.dart
@@ -1,3 +1,4 @@
+// Synchronous generator
 Iterable<int> naturalsTo(int n) sync* {
   int k = 0;
   while (k < n) {
@@ -5,9 +6,24 @@ Iterable<int> naturalsTo(int n) sync* {
   }
 }
 
+// Generator using recursive calls
+Iterable<int> naturalsDownFrom(int n) sync* {
+  if (n > 0) {
+    yield n;
+    yield* naturalsDownFrom(n - 1);
+  }
+}
+
 void main() {
+  print('---naturalsTo---');
   final values = naturalsTo(5);
   for (var value in values) {
+    print(value);
+  }
+
+  print('---naturalsDownFrom---');
+  final downValues = naturalsDownFrom(5);
+  for (var value in downValues) {
     print(value);
   }
 }
