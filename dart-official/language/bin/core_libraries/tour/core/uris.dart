a5115a5d7bb958ff34dbab0116ce4d81c9776898
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-03-17 08:07:11 +0900

    ✨: Encoding and decoding URI components

--- dart-official/language/bin/core_libraries/tour/core/uris.dart
+++ dart-official/language/bin/core_libraries/tour/core/uris.dart
@@ -1,6 +1,6 @@
 final uri = 'https://example.org/api?foo=some message';
 
-void encodingAndDecoding() {
+void encodingAndDecodingFullyQualifiedURIs() {
   final encoded = Uri.encodeFull(uri);
   assert(encoded == 'https://example.org/api?foo=some%20message');
 
@@ -8,6 +8,15 @@ void encodingAndDecoding() {
   assert(decoded == uri);
 }
 
+void encodingAndDecodingURIComponents() {
+  final encoded = Uri.encodeComponent(uri);
+  assert(encoded == 'https%3A%2F%2Fexample.org%2Fapi%3Ffoo%3Dsome%20message');
+
+  final decoded = Uri.decodeComponent(encoded);
+  assert(decoded == uri);
+}
+
 void main() {
-  encodingAndDecoding();
+  encodingAndDecodingFullyQualifiedURIs();
+  encodingAndDecodingURIComponents();
 }
