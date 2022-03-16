67de076ff016133747688d6ccc719149cad63b9b
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-03-17 08:10:31 +0900

    ✨: Parsing URIs

--- dart-official/language/bin/core_libraries/tour/core/uris.dart
+++ dart-official/language/bin/core_libraries/tour/core/uris.dart
@@ -16,7 +16,19 @@ void encodingAndDecodingURIComponents() {
   assert(decoded == uri);
 }
 
+void parsingURIs() {
+  final uri = Uri.parse('https://example.org:8080/foo/bar#frag');
+
+  assert(uri.scheme == 'https');
+  assert(uri.host == 'example.org');
+  assert(uri.port == 8080);
+  assert(uri.path == '/foo/bar');
+  assert(uri.fragment == 'frag');
+  assert(uri.origin == 'https://example.org:8080');
+}
+
 void main() {
   encodingAndDecodingFullyQualifiedURIs();
   encodingAndDecodingURIComponents();
+  parsingURIs();
 }
