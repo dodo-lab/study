9c44e0e6c1caba48f738b91b17d8743429b51ece
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-03-18 08:44:11 +0900

    ✨: Random

---
+++ dart-official/language/bin/core_libraries/tour/math/random.dart
@@ -0,0 +1,23 @@
+import 'dart:math';
+
+void main() {
+  final random = Random();
+  final secureRandom = Random.secure();
+
+  final doubles = <double>[];
+  final ints = <int>[];
+  final bools = <bool>[];
+  final secureDoubles = <double>[];
+
+  for (var i = 0; i < 5; i++) {
+    doubles.add(random.nextDouble()); // Between 0.0 and 1.0.
+    ints.add(random.nextInt(10)); // Between 0 nad 9.
+    bools.add(random.nextBool()); // true or false
+    secureDoubles.add(secureRandom.nextDouble());
+  }
+
+  print('doubles: $doubles');
+  print('ints: $ints');
+  print('bools: $bools');
+  print('secureDoubles: $secureDoubles');
+}
