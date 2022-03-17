0b4658c88b50bce40476ff4d80acb44e23e45149
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-03-18 07:12:01 +0900

    ✨: Using an asynchronous for loop

---
+++ dart-official/language/bin/core_libraries/tour/async/stream/using_an_asynchronous_for_loop.dart
@@ -0,0 +1,32 @@
+import 'dart:io';
+
+void searchFilesByStream(String path) async {
+  print('----searchFilesByStream');
+  FileSystemEntity.isDirectory(path).then((isDir) {
+    if (isDir) {
+      final startingDir = Directory(path);
+      startingDir.list().listen((entity) {
+        if (entity is File) {
+          print(entity.path);
+        }
+      });
+    }
+  });
+}
+
+Future<void> searchFilesByAwait(String path) async {
+  print('----searchFilesByAwait');
+  if (await FileSystemEntity.isDirectory(path)) {
+    final startingDir = Directory(path);
+    await for (final entity in startingDir.list()) {
+      if (entity is File) {
+        print(entity.path);
+      }
+    }
+  }
+}
+
+void main() async {
+  await searchFilesByAwait('./');
+  searchFilesByStream('./');
+}
