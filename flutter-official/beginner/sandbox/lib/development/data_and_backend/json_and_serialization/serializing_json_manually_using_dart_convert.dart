87fad36dced161c85f3dd4c9faa95254b743a6b1
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-03-26 12:02:59 +0900

    ♻️: Common widget group containers

--- flutter-official/beginner/sandbox/lib/development/data_and_backend/json_and_serialization/serializing_json_manually_using_dart_convert.dart
+++ flutter-official/beginner/sandbox/lib/development/data_and_backend/json_and_serialization/serializing_json_manually_using_dart_convert.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:sandbox/widgets/widgets_with_title.dart';
 
 class User {
   final String name;
@@ -36,37 +37,18 @@ class SerializingJsonManuallyUsingDartConvert extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            buildContainer('Decode from json to User class.', [
+            buildWidgetsWithTitle('Decode from json to User class.', [
               Text('Howdy, ${user.name}!'),
               Text('We sent the verification link to ${user.email}.'),
             ]),
-            buildContainer('User class to json.', [
+            buildWidgetsWithTitle('User class to json.', [
               Text('${user.toJson()}'),
             ]),
-            buildContainer('Encode from User class to json.', [
+            buildWidgetsWithTitle('Encode from User class to json.', [
               Text(jsonEncode(user)),
             ]),
           ],
         ),
-      ),
-    );
-  }
-
-  Container buildContainer(String title, List<Widget> widgets) {
-    return Container(
-      margin: const EdgeInsets.symmetric(vertical: 10.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            title,
-            style: const TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.blue,
-            ),
-          ),
-          ...widgets
-        ],
       ),
     );
   }
