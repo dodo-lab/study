45cd6cf6a57327f0294a77dd3c1115795cfea9eb
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-03-31 08:59:09 +0900

    ✨: AppBarを設置

--- flutter-official/beginner/material-components-flutter-codelabs_100_series/lib/home.dart
+++ flutter-official/beginner/material-components-flutter-codelabs_100_series/lib/home.dart
@@ -23,10 +23,42 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: Return an AsymmetricView (104)
     // TODO: Pass Category variable to AsymmetricView (104)
-    return const Scaffold(
-      // TODO: Add app bar (102)
+    return Scaffold(
+      // FIXED: Add app bar (102)
+      appBar: AppBar(
+        title: const Text('SHRINE'),
+        leading: IconButton(
+          icon: const Icon(
+            Icons.menu,
+            semanticLabel: 'menu',
+          ),
+          onPressed: () {
+            print('Menu button');
+          },
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.search,
+              semanticLabel: 'search',
+            ),
+            onPressed: () {
+              print('Search button');
+            },
+          ),
+          IconButton(
+            icon: const Icon(
+              Icons.tune,
+              semanticLabel: 'filter',
+            ),
+            onPressed: () {
+              print('Filter button');
+            },
+          ),
+        ],
+      ),
       // TODO: Add a grid view (102)
-      body: Center(
+      body: const Center(
         child: Text('You did it!'),
       ),
       // FIXED: Set resizeToAvoidBottomInset (101)
