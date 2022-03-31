7300dde7745ef80d64c3659d8dde9a2292e880d4
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-03-31 09:17:31 +0900

    ✨: カード作成処理をAPIに切り出して、複数のカードを作成する

--- flutter-official/beginner/material-components-flutter-codelabs_100_series/lib/home.dart
+++ flutter-official/beginner/material-components-flutter-codelabs_100_series/lib/home.dart
@@ -17,7 +17,37 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
-  // TODO: Make a collection of cards (102)
+  // FIXED: Make a collection of cards (102)
+  List<Card> _buildGridCards(int count) {
+    List<Card> cards = List.generate(
+        count,
+        (index) => Card(
+              clipBehavior: Clip.antiAlias,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  AspectRatio(
+                    aspectRatio: 18.0 / 11.0,
+                    child: Image.asset('assets/diamond.png'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text('Title'),
+                        SizedBox(height: 8.0),
+                        Text('Secondary Text')
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ));
+
+    return cards;
+  }
+
   // TODO: Add a variable for Category (104)
   @override
   Widget build(BuildContext context) {
@@ -62,31 +92,7 @@ class HomePage extends StatelessWidget {
         crossAxisCount: 2,
         padding: const EdgeInsets.all(16.0),
         childAspectRatio: 8.0 / 9.0,
-        children: [
-          Card(
-            clipBehavior: Clip.antiAlias,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                AspectRatio(
-                  aspectRatio: 18.0 / 11.0,
-                  child: Image.asset('assets/diamond.png'),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(16.0, 12.0, 16.0, 8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: const [
-                      Text('Title'),
-                      SizedBox(height: 8.0),
-                      Text('Secondary Text')
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
-        ],
+        children: _buildGridCards(10),
       ),
       // FIXED: Set resizeToAvoidBottomInset (101)
       resizeToAvoidBottomInset: false,
