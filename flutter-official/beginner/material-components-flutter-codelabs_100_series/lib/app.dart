19f1a40020b2effe443496cf913813ecc9d365ae
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-04-01 06:41:46 +0900

    ✨: テーマカラーのカスタマイズ

--- flutter-official/beginner/material-components-flutter-codelabs_100_series/lib/app.dart
+++ flutter-official/beginner/material-components-flutter-codelabs_100_series/lib/app.dart
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 import 'package:flutter/material.dart';
+import 'package:shrine/colors.dart';
 
 import 'home.dart';
 import 'login.dart';
@@ -32,7 +33,8 @@ class ShrineApp extends StatelessWidget {
       // TODO: Change backLayer field value to CategoryMenuPage (104)
       initialRoute: '/login',
       onGenerateRoute: _getRoute,
-      // TODO: Add a theme (103)
+      // FIXED: Add a theme (103)
+      theme: _kShrineTheme,
     );
   }
 
@@ -49,5 +51,22 @@ class ShrineApp extends StatelessWidget {
   }
 }
 
-// TODO: Build a Shrine Theme (103)
+// FIXED: Build a Shrine Theme (103)
+final ThemeData _kShrineTheme = _buildShrineTheme();
+
+ThemeData _buildShrineTheme() {
+  final ThemeData base = ThemeData.light();
+  return base.copyWith(
+    colorScheme: base.colorScheme.copyWith(
+      primary: kShrinePink100,
+      onPrimary: kShrineBrown900,
+      secondary: kShrineBrown900,
+      error: kShrineErrorRed,
+    ),
+    // TODO: Add the text themes (103)
+    // TODO: Add the icon themes (103)
+    // TODO: Decorate the inputs (103)
+  );
+}
+
 // TODO: Build a Shrine Text Theme (103)
