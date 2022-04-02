c80f7bbf589a63e8577d987a0fe6e02b7da3c560
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-04-02 11:51:04 +0900

    ✨: メニューアイコンとテキストをアニメーション制御

--- flutter-official/beginner/material-components-flutter-codelabs_100_series/lib/backdrop.dart
+++ flutter-official/beginner/material-components-flutter-codelabs_100_series/lib/backdrop.dart
@@ -62,7 +62,89 @@ class _FrontLayer extends StatelessWidget {
   }
 }
 
-// TODO: Add _BackdropTitle class (104)
+// FIXED: Add _BackdropTitle class (104)
+class _BackdropTitle extends AnimatedWidget {
+  final void Function() onPress;
+  final Widget frontTitle;
+  final Widget backTitle;
+
+  const _BackdropTitle({
+    Key? key,
+    required Animation<double> listenable,
+    required this.onPress,
+    required this.frontTitle,
+    required this.backTitle,
+  })  : _listenable = listenable,
+        super(key: key, listenable: listenable);
+
+  final Animation<double> _listenable;
+
+  @override
+  Widget build(BuildContext context) {
+    final Animation<double> animation = _listenable;
+
+    return DefaultTextStyle(
+      style: Theme.of(context).textTheme.headline6!,
+      softWrap: false,
+      overflow: TextOverflow.ellipsis,
+      child: Row(
+        children: [
+          // brand icon
+          SizedBox(
+            width: 72.0,
+            child: IconButton(
+              padding: const EdgeInsets.only(right: 8.0),
+              onPressed: onPress,
+              icon: Stack(children: [
+                Opacity(
+                  opacity: animation.value,
+                  child: const ImageIcon(AssetImage('assets/slanted_menu.png')),
+                ),
+                FractionalTranslation(
+                  translation: Tween<Offset>(
+                    begin: Offset.zero,
+                    end: const Offset(1.0, 0.0),
+                  ).evaluate(animation),
+                  child: const ImageIcon(AssetImage('assets/diamond.png')),
+                ),
+              ]),
+            ),
+          ),
+          Stack(
+            children: [
+              Opacity(
+                opacity: CurvedAnimation(
+                  parent: ReverseAnimation(animation),
+                  curve: const Interval(0.5, 1.0),
+                ).value,
+                child: FractionalTranslation(
+                  translation: Tween<Offset>(
+                    begin: Offset.zero,
+                    end: const Offset(0.5, 0.0),
+                  ).evaluate(animation),
+                  child: backTitle,
+                ),
+              ),
+              Opacity(
+                opacity: CurvedAnimation(
+                  parent: animation,
+                  curve: const Interval(0.5, 1.0),
+                ).value,
+                child: FractionalTranslation(
+                  translation: Tween<Offset>(
+                    begin: const Offset(-0.25, 0.0),
+                    end: Offset.zero,
+                  ).evaluate(animation),
+                  child: frontTitle,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
 
 // FIXED: Add _BackdropState class (104)
 class _BackdropState extends State<Backdrop>
@@ -153,13 +235,14 @@ class _BackdropState extends State<Backdrop>
       elevation: 0.0,
       titleSpacing: 0.0,
       // FIXED: Replace leading menu icon with IconButton (104)
-      // TODO: Remove leading property (104)
-      // TODO: Create title with _BackdropTitle parameter (104)
-      leading: IconButton(
-        icon: const Icon(Icons.menu),
-        onPressed: _toggleBackdropLayerVisibility,
+      // FIXED: Remove leading property (104)
+      // FIXED: Create title with _BackdropTitle parameter (104)
+      title: _BackdropTitle(
+        listenable: _controller.view,
+        onPress: _toggleBackdropLayerVisibility,
+        frontTitle: widget.frontTitle,
+        backTitle: widget.backTitle,
       ),
-      title: const Text('SHRINE'),
       actions: [
         // TODO: Add shortcut to login screen from trailing icons (104)
         IconButton(
