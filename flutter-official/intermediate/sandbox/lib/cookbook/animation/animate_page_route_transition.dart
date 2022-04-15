26b0ce5428fef2364d8e3fea5193608f10598b1e
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-04-15 19:55:11 +0900

    ✨: Page animation

--- flutter-official/intermediate/sandbox/lib/cookbook/animation/animate_page_route_transition.dart
+++ flutter-official/intermediate/sandbox/lib/cookbook/animation/animate_page_route_transition.dart
@@ -24,7 +24,15 @@ Route _createRoute() {
       pageBuilder: (context, animation, secondaryAnimation) =>
           const AnimatePageRouteTransition2(),
       transitionsBuilder: (context, animation, secondaryAnimation, child) {
-        return child;
+        const begin = Offset(0.0, 1.0);
+        const end = Offset.zero;
+        final tween = Tween(begin: begin, end: end);
+        final offsetAnimation = animation.drive(tween);
+
+        return SlideTransition(
+          position: offsetAnimation,
+          child: child,
+        );
       });
 }
 
