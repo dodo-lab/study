de0e3ec33022f736dbde5c4debfbf70833c352cb
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-04-23 12:04:57 +0900

    ✨: Implement the plugin platform code

--- flutter-official/intermediate/plugin_codelab/android/src/main/java/com/example/plugin_codelab/PluginCodelabPlugin.java
+++ flutter-official/intermediate/plugin_codelab/android/src/main/java/com/example/plugin_codelab/PluginCodelabPlugin.java
@@ -1,12 +1,16 @@
 package com.example.plugin_codelab;
 
+import java.util.ArrayList;
+
 import androidx.annotation.NonNull;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /** PluginCodelabPlugin */
 public class PluginCodelabPlugin implements FlutterPlugin, MethodCallHandler {
@@ -15,17 +19,41 @@ public class PluginCodelabPlugin implements FlutterPlugin, MethodCallHandler {
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
   private MethodChannel channel;
+  private Synth synth;
+  private static final String channelName = "plugin_codelab";
+
+  private static void setup(PluginCodelabPlugin plugin, BinaryMessenger binaryMessenger) {
+    plugin.channel = new MethodChannel(binaryMessenger, channelName);
+    plugin.channel.setMethodCallHandler(plugin);
+    plugin.synth = new Synth();
+    plugin.synth.start();
+  }
 
   @Override
   public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
-    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "plugin_codelab");
-    channel.setMethodCallHandler(this);
+    setup(this, flutterPluginBinding.getBinaryMessenger());
   }
 
   @Override
   public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
     if (call.method.equals("getPlatformVersion")) {
       result.success("Android " + android.os.Build.VERSION.RELEASE);
+    } else if (call.method.equals("onKeyDown")) {
+      try {
+        ArrayList arguments = (ArrayList) call.arguments;
+        int numKeysDown = synth.keyDown((Integer) arguments.get(0));
+        result.success(numKeysDown);
+      } catch (Exception ex) {
+        result.error("1", ex.getMessage(), ex.getStackTrace());
+      }
+    } else if (call.method.equals("onKeyUp")) {
+      try {
+        ArrayList arguments = (ArrayList) call.arguments;
+        int numKeysUp = synth.keyUp((Integer) arguments.get(0));
+        result.success(numKeysUp);
+      } catch (Exception ex) {
+        result.error("1", ex.getMessage(), ex.getStackTrace());
+      }
     } else {
       result.notImplemented();
     }
