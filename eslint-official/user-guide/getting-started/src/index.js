50f8209edb36a781aad3f13ee8b25ce219ba446a
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-04-26 09:12:05 +0900

    🔨: ESLintの実行をスクリプト化

---
+++ eslint-official/user-guide/getting-started/src/index.js
@@ -0,0 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+// セミコロン無しでESLintエラー.
+const noSemicolon = 0
+
+// シングルクォーテーションでESLintエラー.
+const singleQuote = 'bbb';
+
+// ダブルクオーテーションはOK.
+const doubleQuote = "aaa";
