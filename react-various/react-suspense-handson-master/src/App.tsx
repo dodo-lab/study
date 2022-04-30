1b1594026a9baf5671b8bc9c3a597f8f65ee14c4
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-05-01 07:55:35 +0900

    ✨: SometimesSuspend（1/2の確率でthrow Promiseかコンポーネントを返却）

--- react-various/react-suspense-handson-master/src/App.tsx
+++ react-various/react-suspense-handson-master/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from 'react';
+import React, { Suspense, useState } from 'react';
 import './App.css'
 
 function sleep(ms: number) {
@@ -6,15 +6,28 @@ function sleep(ms: number) {
 }
 
 const AlwaysSuspend: React.VFC = () => {
+  console.log('AlwaysSuspend is rendered');
   throw sleep(1000);
 }
 
+const SometimesSuspend: React.VFC = () => {
+  if(Math.random() < 0.5) {
+    throw sleep(1000);
+  }
+  return <p>Hello, world!</p>
+}
+
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="text-center">
       <h1 className="text-2xl">React App!</h1>
       <Suspense fallback={<p>Loading...</p>}>
-        <AlwaysSuspend />
+        <SometimesSuspend />
+        <button className='border p-1' onClick={() => setCount((c) => c + 1)}>
+          {count}
+        </button>
       </Suspense>
     </div>
   )
