9d17cd7b803c9ec63e9956ff262b8a15b6e6d6e6
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-05-01 08:04:11 +0900

    ✨: RenderingNotifierでSuspenseの内外でのレンダリングタイミング差異を確認

--- react-various/react-suspense-handson-master/src/App.tsx
+++ react-various/react-suspense-handson-master/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { Suspense, useState } from 'react';
 import './App.css'
+import { RenderingNotifier } from './components/RenderingNotifier';
 import { SometimesSuspend } from './components/SometimesSuspend';
 
 function App() {
@@ -8,8 +9,10 @@ function App() {
   return (
     <div className="text-center">
       <h1 className="text-2xl">React App!</h1>
+      <RenderingNotifier name="outside-Suspend" />
       <Suspense fallback={<p>Loading...</p>}>
         <SometimesSuspend />
+        <RenderingNotifier name="inside-Suspend" />
         <button className='border p-1' onClick={() => setCount((c) => c + 1)}>
           {count}
         </button>
