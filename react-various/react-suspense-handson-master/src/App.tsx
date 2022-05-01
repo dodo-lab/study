31d835765946924b988273aa2a91443f654d0f91
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-05-01 10:23:52 +0900

    ✨: Mapで複数のデータを保持

--- react-various/react-suspense-handson-master/src/App.tsx
+++ react-various/react-suspense-handson-master/src/App.tsx
@@ -1,6 +1,6 @@
 import {Suspense, useState} from 'react';
 import './App.css';
-import {DataLoader} from './components/DataLoader';
+import {DataLoader, DataLoader2} from './components/DataLoader';
 import {RenderingNotifier} from './components/RenderingNotifier';
 
 function App() {
@@ -12,6 +12,7 @@ function App() {
       <RenderingNotifier name="outside-Suspend" />
       <Suspense fallback={<p>Loading...</p>}>
         <DataLoader />
+        <DataLoader2 />
         <RenderingNotifier name="inside-Suspend" />
         <button className="border p-1" onClick={() => setCount(c => c + 1)}>
           {count}
--- react-various/react-suspense-handson-master/src/components/DataLoader.tsx
+++ react-various/react-suspense-handson-master/src/components/DataLoader.tsx
@@ -1,17 +1,28 @@
 import {fetchData1} from '../utils';
 
-let data: string | undefined;
+let dataMap = new Map<string, string>();
 
-function useData1(): string {
-  if (data === undefined) {
-    throw fetchData1().then(d => (data = d));
+function useData1(cacheKey: string): string {
+  const cacheData = dataMap.get(cacheKey);
+  if (cacheData === undefined) {
+    throw fetchData1().then(d => dataMap.set(cacheKey, d));
   }
 
-  return data;
+  return cacheData;
 }
 
 export const DataLoader: React.VFC = () => {
-  const data = useData1();
+  const data = useData1('DataLoader1');
+
+  return (
+    <>
+      <div>Data is {data}</div>
+    </>
+  );
+};
+
+export const DataLoader2: React.VFC = () => {
+  const data = useData1('DataLoader2');
 
   return (
     <>
