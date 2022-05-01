e867f733c1c005be0b78eee74703f992ec63163d
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-05-01 09:59:03 +0900

    ⚗️: マウント後にサスペンド

--- react-various/react-suspense-handson-master/src/components/DataLoader.tsx
+++ react-various/react-suspense-handson-master/src/components/DataLoader.tsx
@@ -2,11 +2,19 @@ import { useState } from "react";
 import { fetchData1 } from "../utils";
 
 export const DataLoader: React.VFC = () => {
+  const [loading, setLoading] = useState(false);
   const [data, setData] = useState<string | null>(null);
 
-  if(data === null) {
+  if (loading && data === null) {
     throw fetchData1().then(setData);
   }
 
-  return <div>Data is {data}</div>
-}
+  return (
+    <>
+      <div>Data is {data}</div>
+      <button className="border p-1" onClick={() => setLoading(true)}>
+        load
+      </button>
+    </>
+  );
+};
