988c45492ea6b7a4ae6aab000750c55b319769a1
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-06-08 07:12:24 +0900

    ✨: アプリ起動と終了の実装

---
+++ electron-official/quick-start/main.js
@@ -0,0 +1,30 @@
+const {app, BrowserWindow} = require('electron');
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  win.loadFile('index.html');
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  // アクティベート時にウィンドウがなければ作成する(Mac向けの対応).
+  // https://www.electronjs.org/docs/latest/tutorial/quick-start#open-a-window-if-none-are-open-macos
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+// 全てのウィンドウを閉じた時にアプリを終了(WindowsとLinux向けの対応).
+// https://www.electronjs.org/docs/latest/tutorial/quick-start#quit-the-app-when-all-windows-are-closed-windows--linux
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
