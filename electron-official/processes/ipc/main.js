ab75c2026b937ea9f4fe651f30628110c577020a
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-06-08 09:10:40 +0900

    ✨: レンダラーからメインへIPC通信

---
+++ electron-official/processes/ipc/main.js
@@ -0,0 +1,34 @@
+const {app, BrowserWindow, ipcMain} = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+    },
+  });
+
+  ipcMain.on('set-title', (event, title) => {
+    const webContents = event.sender;
+    const win = BrowserWindow.fromWebContents(webContents);
+    win.setTitle(title);
+  });
+
+  mainWindow.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
