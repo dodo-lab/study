83aad16e3d01b613f672a3e12c6294b0a3daf7a1
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-07-17 07:57:50 +0900

    ✨: playground middleware

--- first-graphql/photo-share-api-express/index.js
+++ first-graphql/photo-share-api-express/index.js
@@ -1,3 +1,5 @@
+const expressPlayground =
+  require('graphql-playground-middleware-express').default;
 const { ApolloServer } = require('apollo-server-express');
 const express = require('express');
 const { GraphQLScalarType } = require('graphql');
@@ -157,13 +159,15 @@ async function main() {
   server.applyMiddleware({ app });
 
   app.get('/', (req, res) => res.end('Welcome to the PhotoShare API'));
+  app.get('/playground', expressPlayground({ endpoint: '/graphql' }));
 
   // Webサーバーを起動
-  app.listen({ port: 4000 }, () =>
+  app.listen({ port: 4000 }, () => {
     console.log(
       `GraphQL Service running @ http://localhost:4000${server.graphqlPath}`
-    )
-  );
+    );
+    console.log(`GraphQL Playground @ http://localhost:4000/playground`);
+  });
 }
 
 main();
