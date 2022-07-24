29a8703bc14944752d6e1fc0cf777b2963440203
Wataru Shindo <40927126+dodo-lab@users.noreply.github.com>
2022-07-25 05:52:00 +0900

    ✨: graphql-requestでmutationを実行

--- first-graphql/photo-share-client/src/pages/graphql-request.tsx
+++ first-graphql/photo-share-client/src/pages/graphql-request.tsx
@@ -1,4 +1,4 @@
-import {Box, Button, Container, Typography} from '@mui/material';
+import {Box, Button, Container, Stack, Typography} from '@mui/material';
 import {GRAPHQL_URL} from 'constants/graphql';
 import request from 'graphql-request';
 import type {NextPage} from 'next';
@@ -13,6 +13,15 @@ const query = `
   }
 `;
 
+const mutation = `
+  mutation populate($count: Int!) {
+    addFakeUsers(count: $count) {
+      githubLogin
+      name
+    }
+  }
+`;
+
 type User = {
   name: string;
   avatar: string | null;
@@ -21,21 +30,33 @@ type User = {
 const Page: NextPage = () => {
   const [users, setUsers] = useState<User[]>([]);
 
-  const getData = useCallback(() => {
+  const getUsers = useCallback(() => {
     request(GRAPHQL_URL, query).then(data => setUsers(data.allUsers));
   }, []);
 
+  const populate = useCallback(() => {
+    const params = {count: 1};
+    request(GRAPHQL_URL, mutation, params).then(_ => getUsers());
+  }, [getUsers]);
+
   return (
     <Container maxWidth="xl">
-      <Button variant="contained" onClick={getData}>
-        get
-      </Button>
-      {users.map((user, index) => (
-        <Box key={index} sx={{borderBottom: 1}}>
-          <Typography>{user.name}</Typography>
-          {user.avatar && <Typography>{user.avatar}</Typography>}
-        </Box>
-      ))}
+      <Box sx={{mt: 1}}>
+        <Stack direction="row" sx={{mb: 1, gap: 1}}>
+          <Button variant="contained" onClick={getUsers}>
+            get users
+          </Button>
+          <Button variant="contained" onClick={populate}>
+            populate
+          </Button>
+        </Stack>
+        {users.map((user, index) => (
+          <Box key={index} sx={{borderBottom: 1}}>
+            <Typography>{user.name}</Typography>
+            {user.avatar && <Typography>{user.avatar}</Typography>}
+          </Box>
+        ))}
+      </Box>
     </Container>
   );
 };
