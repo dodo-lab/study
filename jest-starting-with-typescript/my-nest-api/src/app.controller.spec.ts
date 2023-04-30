cbcbd55505ec56e494ad15bcf4823e7d8e6bc636
w-shindo <mujet88x@gmail.com>
2023-05-01 06:43:23 +0900

    ✅: hello api test

--- jest-starting-with-typescript/my-nest-api/src/app.controller.spec.ts
+++ jest-starting-with-typescript/my-nest-api/src/app.controller.spec.ts
@@ -6,17 +6,20 @@ describe('AppController', () => {
   let appController: AppController;
 
   beforeEach(async () => {
+    // テスト用のNestJSアプリケーションを作成する
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
       providers: [AppService],
     }).compile();
 
+    // アプリケーションからAppControllerを取得する
     appController = app.get<AppController>(AppController);
   });
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+      // getHello() メソッドが'Hello from NestJS API!'を返すことを確認する
+      expect(appController.getHello()).toBe('Hello from NestJS API!');
     });
   });
 });
